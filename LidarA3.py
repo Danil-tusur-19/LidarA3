589be0c8cbbeddd8eb7448f27739612f2f980ae2
Danil-tusur-19 <mamba_crytt@mail.ru>
2025-03-03 13:12:01 +0700

    Реализован переход в декартовую систему координат и реализован пример вывода графика

--- LidarA3.py
+++ LidarA3.py
@@ -6,15 +6,11 @@ lidar = PyRPlidar()
 lidar.connect(port="COM4", baudrate=256000, timeout=3)
 # Windows : "COM4"
 
-
 info = lidar.get_info()
 print("info :", info)
 
-
-
 x = []
 y = []
-
 
 lidar.set_motor_pwm(500)
 time.sleep(2)
@@ -22,56 +18,27 @@ time.sleep(2)
 scan_generator = lidar.start_scan_express(4)
 
 for count, scan in enumerate(scan_generator()):
+    x.append(scan.distance * math.cos(math.radians(scan.angle)))
+    y.append(scan.distance * math.sin(math.radians(scan.angle)))
     print(count, scan.angle, scan.distance)
-    if count == 10: break
-
-lidar.stop()
-lidar.set_motor_pwm(0)
-
-lidar.disconnect()
-
+    if count == 250:
+        lidar.stop()
+        lidar.set_motor_pwm(0)
+        lidar.disconnect()
+        break
 
 
+print("Переведенные координаты по оси X:",x)
+print("Переведенные координаты по оси Y:",y)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#import numpy as np
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 #from matplotlib.animation import FuncAnimation
 
-#fig, ax = plt.subplots()
+plt.figure()
+plt.scatter(x,y)
 
-#x = np.linspace(1, 100, 100)
-#y = np.random.randint(1, 100, size=100)  # Инициализация y
-#line, = ax.plot(x, y)
-
-#def animation(i):
-    #y = np.random.randint(1, 100, size=100)  # Обновление y
-    #line.set_ydata(y)  # Обновление данных линии
-    #return line,  # Возвращаем линию для обновления
-
-#an = FuncAnimation(fig, animation, frames=100, interval=50, blit=True)
-
-#plt.xlabel('Ось х')  # Подпись для оси х
-#plt.ylabel('Ось y')  # Подпись для оси y
-#plt.title('Первый график')  # Название
-#plt.show()
+plt.xlabel('Ось х')  # Подпись для оси х
+plt.ylabel('Ось y')  # Подпись для оси y
+plt.title('Первый график')  # Название
+plt.show()
